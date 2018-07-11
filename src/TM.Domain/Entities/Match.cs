5bcb35030650900a4858e53d808b3645d9f78281
REDSPARK\romulo.martins <romulo.martins@redspark.io>
2018-07-11 14:50:21 -0300

    Implementando classe Match e Testando

---
+++ src/TM.Domain/Entities/Match.cs
@@ -0,0 +1,77 @@
+﻿
+namespace TM.Domain.Entities
+{
+    using System;
+    using Core.Models;
+
+    public class Match : Entity<Match>
+    {
+        public Team TeamA { get; private set; }
+        public Team TeamB { get; private set; }
+        public Team TeamWinner { get; private set; }
+
+        private Match() { }
+
+        public Match(Team teamA, Team teamB)
+        {
+            TeamA = teamA;
+            TeamB = teamB;
+        }
+
+        public Match TeamAWinner()
+        {
+            TeamWinner = TeamA;
+            return this;
+        }
+
+        public Match TeamBWinner()
+        {
+            TeamWinner = TeamB;
+            return this;
+        }
+
+        public override bool IsValid()
+        {
+            MinimumValidate();
+
+            return ValidationResult.IsValid;
+        }
+
+        #region Validate
+        private void MinimumValidate()
+        {
+            ValidationResult = Validate(this);
+
+            ValidateTeamA();
+            ValidateTeamB();
+        }
+
+        private void ValidateTeamA()
+        {
+            if (TeamA.IsValid()) return;
+            foreach (var error in TeamA.ValidationResult.Errors)
+            {
+                ValidationResult.Errors.Add(error);
+            }
+        }
+
+        private void ValidateTeamB()
+        {
+            if (TeamB.IsValid()) return;
+            foreach (var error in TeamB.ValidationResult.Errors)
+            {
+                ValidationResult.Errors.Add(error);
+            }
+        }
+        #endregion
+
+        #region Factory
+        public class MatchFactory
+        {
+            public static Match NewCompleteMatch(Guid id, Team teamA, Team teamB) => new Match() { Id = id, TeamA = teamA, TeamB = teamB };
+        }
+
+
+        #endregion
+    }
+}
---
+++ tests/TM.DomainTests/Models/MatchTests.cs
@@ -0,0 +1,98 @@
+﻿
+namespace TM.DomainTests.Models
+{
+    using System;
+    using TM.Domain.Entities;
+    using Xunit;
+
+    public class MatchTests
+    {
+        [Fact]
+        [Trait("Match", "Who Won Method")]
+        public void WhenChooseTheTeamAAsTheWinnerThenTheMatchMustHaveTheWinnerEqualTeamA()
+        {
+            var teamA = Team.TeamFactory.NewCompleteTeam(Guid.NewGuid(), "FLA", "Flamengo");
+            var teamB = Team.TeamFactory.NewCompleteTeam(Guid.NewGuid(), "VAS", "Vasco");
+            Match match = Match.MatchFactory.NewCompleteMatch(Guid.NewGuid(), teamA, teamB);
+
+            match.TeamAWinner();
+
+            Assert.Equal(teamA, match.TeamWinner);
+        }
+
+        [Fact]
+        [Trait("Match", "Who Won Method")]
+        public void WhenChooseTheTeamBAsTheWinnerThenTheMatchMustHaveTheWinnerEqualTeamB()
+        {
+            var teamA = Team.TeamFactory.NewCompleteTeam(Guid.NewGuid(), "FLA", "Flamengo");
+            var teamB = Team.TeamFactory.NewCompleteTeam(Guid.NewGuid(), "VAS", "Vasco");
+            Match match = Match.MatchFactory.NewCompleteMatch(Guid.NewGuid(), teamA, teamB);
+
+            match.TeamBWinner();
+
+            Assert.Equal(teamB, match.TeamWinner);
+        }
+
+        #region Creation Tests
+        [Fact]
+        [Trait("Match", "Creation Tests")]
+        public void MatchCreatedByFactoryWithAllValuesMustBeValid()
+        {
+            var teamA = Team.TeamFactory.NewCompleteTeam(Guid.NewGuid(), "FLA", "Flamengo");
+            var teamB = Team.TeamFactory.NewCompleteTeam(Guid.NewGuid(), "VAS", "Vasco");
+            Match match = Match.MatchFactory.NewCompleteMatch(Guid.NewGuid(), teamA, teamB);
+
+            Assert.NotNull(match.TeamA);
+            Assert.NotNull(match.TeamB);
+            Assert.Equal(teamA, match.TeamA);
+            Assert.Equal(teamB, match.TeamB);
+            Assert.True(match.IsValid());
+        }
+
+        [Fact]
+        [Trait("Match", "Creation Tests")]
+        public void WhenMatchCreatedWithInvalidTeamATheMatchMustBeInvalid()
+        {
+            var teamA = Team.TeamFactory.NewCompleteTeam(Guid.NewGuid(), "F", "Flamengo");
+            var teamB = Team.TeamFactory.NewCompleteTeam(Guid.NewGuid(), "VAS", "Vasco");
+            Match match = Match.MatchFactory.NewCompleteMatch(Guid.NewGuid(), teamA, teamB);
+
+            Assert.NotNull(match.TeamA);
+            Assert.NotNull(match.TeamB);
+            Assert.Equal(teamA, match.TeamA);
+            Assert.Equal(teamB, match.TeamB);
+            Assert.False(match.IsValid());
+        }
+
+        [Fact]
+        [Trait("Match", "Creation Tests")]
+        public void WhenMatchCreatedWithInvalidTeamBTheMatchMustBeInvalid()
+        {
+            var teamA = Team.TeamFactory.NewCompleteTeam(Guid.NewGuid(), "FLA", "Flamengo");
+            var teamB = Team.TeamFactory.NewCompleteTeam(Guid.NewGuid(), "V", "Vasco");
+            Match match = Match.MatchFactory.NewCompleteMatch(Guid.NewGuid(), teamA, teamB);
+
+            Assert.NotNull(match.TeamA);
+            Assert.NotNull(match.TeamB);
+            Assert.Equal(teamA, match.TeamA);
+            Assert.Equal(teamB, match.TeamB);
+            Assert.False(match.IsValid());
+        }
+
+        [Fact]
+        [Trait("Match", "Creation Tests")]
+        public void MatchCreatedByConstructorWithAllValuesMinimumMustBeValid()
+        {
+            var teamA = Team.TeamFactory.NewCompleteTeam(Guid.NewGuid(), "FLA", "Flamengo");
+            var teamB = Team.TeamFactory.NewCompleteTeam(Guid.NewGuid(), "VAS", "Vasco");
+            Match match = new Match(teamA, teamB);
+
+            Assert.NotNull(match.TeamA);
+            Assert.NotNull(match.TeamB);
+            Assert.Equal(teamA, match.TeamA);
+            Assert.Equal(teamB, match.TeamB);
+            Assert.True(match.IsValid());
+        }
+        #endregion
+    }
+}
